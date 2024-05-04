54dcbdfd18b53eaf8530313753dd75156cc5c5e2
4l1fe <4l1fe@users.noreply.github.com>
2024-05-04 05:20:43 +0300

    Draft: comment mode. Dimming, updating, switchinng

--- select_theme.py
+++ select_theme.py
@@ -14,7 +14,7 @@ from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.layout.processors import BeforeInput
 from prompt_toolkit.filters import Condition, has_focus
-from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding import KeyBindings, ConditionalKeyBindings
 from prompt_toolkit.widgets import Label
 from prompt_toolkit.styles import Style
 from prompt_toolkit.buffer import Buffer
@@ -24,19 +24,30 @@ from prompt_toolkit.document import Document
 
 FormattedLine = namedtuple('FormattedLine', ['style', 'string'])
 MAROON_STYLE = 'bg:ansibrightgreen fg:black'
+DIMM_STYLE = 'fg:black'
 DARK_THEME = 'dark'
 LIGHT_THEME = 'light'
 toml_converter = make_converter()
 
 
-class ApplicationState:
-    toggle = True
+class Mode:
+    switch_flag = True
+    
+    @classmethod
+    def is_search_mode(cls):
+        return Mode.switch_flag
+
+    @classmethod
+    def is_comment_mode(cls):
+        return not Mode.switch_flag
+    
+    @classmethod
+    def switch_mode(cls):
+        cls.switch_flag = not cls.switch_flag
     
 
-is_search = lambda: ApplicationState.toggle
-is_comment = lambda: not ApplicationState.toggle
-search_filter = Condition(is_search)
-comment_filter = Condition(is_comment)
+is_search_mode_f = Condition(Mode.is_search_mode)
+is_comment_mode_f = Condition(Mode.is_comment_mode)
 
 
 def to_defaultdict(default_factory, data):
@@ -78,13 +89,12 @@ class FormattedLineString(UserString):
 
     def __init__(self, value: str, pinned: bool = False, comment: str = ''):
         self.value = value
-        self._init_value = value
+        # self._init_value = value
         self._pinned = pinned
         self._pin_char = '*'
         self._comment = comment
 
-        init_data = self._make_data()
-        super().__init__(init_data)
+        super().__init__(self._make_formatted_value())
 
     def toggle_pin(self) -> bool:
         self._pinned = not self._pinned
@@ -100,8 +110,9 @@ class FormattedLineString(UserString):
 
     def update_comment(self, text):
         self._comment = text
+        self._update_data()
 
-    def _make_data(self):
+    def _make_formatted_value(self):
         data = self.value
 
         if self._pinned:
@@ -115,93 +126,114 @@ class FormattedLineString(UserString):
         return data
 
     def _update_data(self):
-        self.data = self._make_data()
+        self.data = self._make_formatted_value()
 
 
-class LineSelector:
+class LineStringSelector:
 
-    def __init__(self, values: list[str], config: SelectorConfig, config_path: Path) -> None:
+    def __init__(self, theme_names: list[str], config: SelectorConfig, config_path: Path) -> None:
         self.config = config
         self.config_path = config_path
-        self.formatted_values = self._create_formatted_lines(values, config.properties)
+        self.formatted_lines = self._create_formatted_lines(theme_names, config.properties)
         self._selected_idx = 0
-        self._selected_line = None
-        self._search_text = ''
+        self._selected_line: FormattedLine = None
+        self._sel_confirmed = False
+        self._typed_text = ''
         self._create_container()
 
-    def get_text_lines(self):
-        if not self.found_values:
-            return self.found_values
+    def get_lines(self):
+        if not self.found_lines:
+            return self.found_lines
 
-        selected_line = self.found_values[self._selected_idx]
+        copied_lines = self.found_lines.copy()
+        copied_lines[self._selected_idx] = FormattedLine(style='[SetCursorPosition]',
+                                                         string=self.selected_line.string)
+        copied_lines = sorted(copied_lines, key=lambda fl: fl.string.is_pinned(), reverse=True)
 
-        copied_values = self.found_values.copy()
-        copied_values[self._selected_idx] = FormattedLine(style='[SetCursorPosition]',
-                                                          string=selected_line.string)
-        copied_values = sorted(copied_values, key=lambda fl: fl.string.is_pinned(), reverse=True)
-
-        return copied_values
+        return copied_lines
 
     def get_selected_value(self):
-        if self._selected_line is None:
-            return self._selected_line
-        
-        return self._selected_line.string.value
+        if self._sel_confirmed:
+            return self.selected_line.string.value
 
-    def get_focus_on(self):
-        if is_search():
+    def get_mode_focus(self) -> BufferControl:
+        if Mode.is_search_mode():
             return self.search_buffer_control
-        elif is_comment():
+        
+        if Mode.is_comment_mode():
             return self.comment_buffer_control
         
-    def text_find(self, buffer: Buffer):
-        self._search_text = buffer.document.text
+    def switch_focus(self):
+        Mode.switch_mode()
+
+        buffc = self.search_buffer_control \
+                if Mode.is_search_mode() \
+                else self.comment_buffer_control   
+        
+        get_app().layout.focus(buffc)
+        
+    def dimm_mode_style(self):
+        if Mode.is_comment_mode():
+            return DIMM_STYLE
+        
+        return '' 
+        
+    def find_text(self, buffer: Buffer):
+        self._typed_text = buffer.document.text
         self._selected_idx = 0
 
     @property
-    def values_len(self) -> int:
-        return len(self.found_values)
+    def values_count(self) -> int:
+        return len(self.found_lines)
 
     @property
-    def found_values(self) -> list[FormattedLine]:
-        values = self.formatted_values
-        if len(self._search_text) > 0:
-            values = [fl for fl in values
-                      if self._search_text in fl.string]
+    def found_lines(self) -> list[FormattedLine]:
+        lines = self.formatted_lines
+        
+        if self._typed_text:
+            lines = [fl for fl in lines
+                      if self._typed_text in fl.string]
 
-        sorted_values = sorted(values, key=lambda fl: fl.string.value.lower())
-        sorted_values = sorted(sorted_values, key=lambda fl: fl.string.is_pinned(), reverse=True)
+        sorted_lines = sorted(lines, key=lambda fl: fl.string.value.lower())
+        sorted_lines = sorted(sorted_lines, key=lambda fl: fl.string.is_pinned(), reverse=True)
 
-        return sorted_values
+        return sorted_lines
+    
+    @property
+    def selected_line(self) -> FormattedLine | None:
+        if self.found_lines:
+            return self.found_lines[self._selected_idx]
 
-    def _create_formatted_lines(self, values, property_lines: dict[LineStringProperties]) -> list[FormattedLine]:
+    def _create_formatted_lines(self, theme_names, theme_props: dict[LineStringProperties]) -> list[FormattedLine]:
         formatted_lines = []
-        for value in values:
-            fls = FormattedLineString(value)
+        for theme_name in theme_names:
+            fl_string = FormattedLineString(theme_name)
 
-            if value in property_lines:
-                lproperties: LineStringProperties = property_lines[value]
-                fls = FormattedLineString(value,
-                                          pinned=lproperties.pinned,
-                                          comment=lproperties.comment)
+            if theme_name in theme_props:
+                props: LineStringProperties = theme_props[theme_name]
+                fl_string = FormattedLineString(theme_name,
+                                                pinned=props.pinned,
+                                                comment=props.comment)
 
-            formatted_lines.append(FormattedLine(style='', string=fls))
+            formatted_lines.append(FormattedLine(style='', string=fl_string))
 
         return formatted_lines
 
     def _create_container(self):
+        has_values = Condition(lambda: bool(self.found_lines))
+        
         kb_select = KeyBindings()
-        has_values = Condition(lambda: bool(self.found_values)
-                               )
+        # kb_select = ConditionalKeyBindings(KeyBindings(), filter=is_search_mode_f)
+
         @kb_select.add('up')
         @kb_select.add('c-k')
-        def kb_up(event):
+        def _up(event):
             self._selected_idx = max(0, self._selected_idx - 1)
 
         @kb_select.add('down')
-        @kb_select.add('c-j')
-        def kb_down(event):
-            self._selected_idx = min(self.values_len - 1, self._selected_idx + 1)
+        @kb_select.add('c-j', filter=is_search_mode_f)
+        def _down(event):
+            self._selected_idx = min(self.values_count - 1, self._selected_idx + 1)
 
         @kb_select.add("pageup")
         @kb_select.add("c-u")
@@ -216,27 +248,46 @@ class LineSelector:
         def _pagedown(event):
             if self.select_window.render_info:
                 self._selected_idx = min(
-                    self.values_len - 1,
+                    self.values_count - 1,
                     self._selected_idx + len(self.select_window.render_info.displayed_lines),
                 )
 
         @kb_select.add('enter')
-        def kb_enter(event):
-            if self.found_values:
-                self._selected_line = self.found_values[self._selected_idx]
+        def confirm_selection(event):
+            if self.selected_line:
+                self._sel_confirmed = True
                 event.app.exit()
 
         @kb_select.add('c-p')
         def pin_unpin(event):
-            if self.found_values:
-                selected_line = self.found_values[self._selected_idx]
+            if self.found_lines:
+                selected_line = self.found_lines[self._selected_idx]
                 pinned = selected_line.string.toggle_pin()
 
                 self.config.properties[selected_line.string.value].pinned = pinned
                 self.config.dump(self.config_path)
+        
+        @kb_select.add('c-l')
+        def switch_comment(event):
+            if self.selected_line:
+                self.comment_buffer_control.buffer.set_document(Document(self.selected_line.string.get_comment()))
+                self.switch_focus()
+            
+        kb_comment = KeyBindings()
+        # kb_comment = ConditionalKeyBindings(KeyBindings(), filter=is_comment_mode_f)
 
-        search_buffer = Buffer(name='searchline',
-                               on_text_changed=self.text_find)
+        @kb_comment.add('c-l')
+        def switch_search(event):
+            self.switch_focus()
+            
+        @kb_comment.add('enter')
+        def update_comment(event):
+            if self.selected_line:
+                self.selected_line.string.update_comment(event.current_buffer.document.text)
+                self.switch_focus()
+        
+        search_buffer = Buffer(name='search-line',
+                               on_text_changed=self.find_text)
         self.search_buffer_control = BufferControl(
                                         buffer=search_buffer,
                                         input_processors=[
@@ -245,24 +296,29 @@ class LineSelector:
                                             ]),
                                         ],
                                         key_bindings=kb_select)
-        search_window = Window(content=self.search_buffer_control, height=1)
-        search_window = ConditionalContainer(search_window,filter=search_filter)
+        search_window = ConditionalContainer(Window(content=self.search_buffer_control,
+                                                    height=1),
+                                             filter=is_search_mode_f)
         
-        comment_buffer = Buffer(name='commentline')  # ConditionalKeyBindings
+        comment_buffer = Buffer(name='comment-line')
         self.comment_buffer_control = BufferControl(
-                                    buffer=comment_buffer,
-                                    input_processors=[
-                                        BeforeInput([
-                                            (MAROON_STYLE, 'Comment:'),
-                                        ]),
-                                    ])
-        comment_window = Window(content=self.comment_buffer_control, height=1)
-        comment_window = ConditionalContainer(comment_window, filter=comment_filter)
+                                        buffer=comment_buffer,
+                                        input_processors=[
+                                            BeforeInput([
+                                                (MAROON_STYLE, 'Comment:'),
+                                            ]),
+                                        ],
+                                        key_bindings=kb_comment)
+        comment_window = ConditionalContainer(Window(content=self.comment_buffer_control,
+                                                     height=1),
+                                              filter=is_comment_mode_f)
         
         self.select_window = Window(content=FormattedTextControl(
-                                                text=self.get_text_lines,
-                                                show_cursor=False,),
-                                    cursorline=True)
+                                                text=self.get_lines,
+                                                show_cursor=False,
+                                                ),
+                                    cursorline=True,
+                                    style=self.dimm_mode_style)
         
         blank_line_window = Window(char=' ', height=1)
 
@@ -282,6 +338,14 @@ style = Style([
 
 
 def select(alacritty_themes_path, selector_config_path):
+    """Run an IO loop, select a line, get a value from one"""
+        
+    theme_paths = list(alacritty_themes_path.iterdir())
+    theme_names = [path.name for path in theme_paths]
+
+    config = SelectorConfig.load(selector_config_path)
+    selector = LineStringSelector(theme_names, config, selector_config_path)
+    
     kb_app = KeyBindings()
 
     @kb_app.add('c-q')
@@ -289,20 +353,9 @@ def select(alacritty_themes_path, selector_config_path):
     def exit_(event):
         event.app.exit()
         
-    theme_paths = list(alacritty_themes_path.iterdir())
-    theme_names = [path.name for path in theme_paths]
-
-    config = SelectorConfig.load(selector_config_path)
-    selector = LineSelector(theme_names, config, selector_config_path)
-
-    @kb_app.add('c-l')
-    def switch(event):
-        ApplicationState.toggle = not ApplicationState.toggle
-        buffc = selector.get_focus_on()
-        get_app().layout.focus(buffc)
-        
     window = HSplit([
                     selector,
+                    # Search help
                     ConditionalContainer(
                         Label([
                            (MAROON_STYLE, 'Search:'), ('', ' Type text to search '),
@@ -311,8 +364,9 @@ def select(alacritty_themes_path, selector_config_path):
                            (MAROON_STYLE, 'Quit:'), ('', ' Ctrl+q/c '),],
                         style='class:label',
                         wrap_lines=False),
-                        filter=search_filter,
+                        filter=is_search_mode_f,
                     ),
+                    # Comment help
                     ConditionalContainer(
                         Label([
                            (MAROON_STYLE, 'Comment:'), ('', ' Write a comment to save '),
@@ -320,7 +374,7 @@ def select(alacritty_themes_path, selector_config_path):
                            (MAROON_STYLE, 'Quit:'), ('', ' Ctrl+q/c '),],
                         style='class:label',
                         wrap_lines=False),
-                        filter=comment_filter,
+                        filter=is_comment_mode_f,
                     ),
                     ],
                     align=VerticalAlign.JUSTIFY)
@@ -340,6 +394,8 @@ def select(alacritty_themes_path, selector_config_path):
 
 
 def write(selected_config_name, alacritty_themes_path, alacritty_config_path, posh_config_path):
+    """Update two toml configs. The Posh theme light, dark mode should correspond the alacritty theme mode"""
+    
     selected_theme_path = alacritty_themes_path / selected_config_name
 
     with open(alacritty_config_path, 'r') as file_a, open(posh_config_path, 'r') as file_p:
